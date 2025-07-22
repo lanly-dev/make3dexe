79c09d6330910ddf4c40c1f9a680bc2f6ffb6d4c
Lan Ly <lanly-dev@users.noreply.github.com>
2025-07-21 19:48:07 -0700

    working 💪

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 python -m venv .venv
 .venv\Scripts\activate
 ```
+
+### Troubleshooting
+>We now require users to upgrade torch to at least v2.6 in order to use the function --> Downgrade `transformer` to 4.49
--- run_hunyuan3d.py
+++ run_hunyuan3d.py
@@ -2,11 +2,11 @@ from hy3dgen.shapegen import Hunyuan3DDiTFlowMatchingPipeline
 from hy3dgen.texgen import Hunyuan3DPaintPipeline
 
 # Generate mesh from image
-pipeline = Hunyuan3DDiTFlowMatchingPipeline.from_pretrained('tencent/Hunyuan3D-2', device='cuda')
+pipeline = Hunyuan3DDiTFlowMatchingPipeline.from_pretrained('tencent/Hunyuan3D-2')
 mesh = pipeline(image='assets/demo.png')[0]
 
 # Generate texture for mesh
-pipeline = Hunyuan3DPaintPipeline.from_pretrained('tencent/Hunyuan3D-2', device='cuda')
+pipeline = Hunyuan3DPaintPipeline.from_pretrained('tencent/Hunyuan3D-2')
 mesh = pipeline(mesh, image='assets/demo.png')
 
 # Save mesh to file (GLB format)
